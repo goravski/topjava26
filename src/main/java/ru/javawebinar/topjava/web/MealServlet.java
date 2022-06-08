4bb8e993e7509a8ff4f58582ce0f2e0fd18e9cc0
Evgen Goravski <evgoravsky@gmail.com>
2022-06-08 10:54:38 +0300

    created list <MealsTo> in Servlet

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,20 +1,47 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealServlet extends HttpServlet {
     private static final Logger log = getLogger(MealServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getAttribute("listMeals") == null){
+            List<Meal> meals = Arrays.asList(
+                    new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500),
+                    new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000),
+                    new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500),
+                    new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100),
+                    new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
+                    new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
+                    new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+            );
+            List<MealTo> mealTo = MealsUtil.filteredByStreams(meals,
+                    LocalTime.of(0, 0), LocalTime.of(23, 59), 2000);
+            req.setAttribute("listMeals", mealTo);
+        }else {
+            List<MealTo> mealTo = (List<MealTo>) req.getAttribute("listMeals");
+            req.setAttribute("listMeals", mealTo);
+        }
         log.debug("redirect to meal");
-        resp.sendRedirect("meals.jsp");
+        getServletContext().getRequestDispatcher("meals.jsp").forward(req,resp);
+
     }
 }
