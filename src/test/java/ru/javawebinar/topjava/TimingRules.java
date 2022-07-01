54b9303080de8b85ccb999925e364abb9789997e
Evgen Goravski <evgoravsky@gmail.com>
2022-07-01 11:23:09 +0300

    5_2_HW4 Refactor added TimingRules

---
+++ src/test/java/ru/javawebinar/topjava/TimingRules.java
@@ -0,0 +1,40 @@
+package ru.javawebinar.topjava;
+
+import org.jboss.logging.Logger;
+import org.junit.AssumptionViolatedException;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.Description;
+
+import java.util.concurrent.TimeUnit;
+
+public class TimingRules {
+    private static final Logger logger = Logger.getLogger("");
+
+    private static void logInfo(Description description, String status, long nanos) {
+        String testName = description.getMethodName();
+        logger.info(String.format("Test %s %s, spent %d microseconds",
+                testName, status, TimeUnit.NANOSECONDS.toMicros(nanos)));
+    }
+
+    public static Stopwatch stopwatch = new Stopwatch() {
+        @Override
+        protected void succeeded(long nanos, Description description) {
+            logInfo(description, "succeeded", nanos);
+        }
+
+        @Override
+        protected void failed(long nanos, Throwable e, Description description) {
+            logInfo(description, "failed", nanos);
+        }
+
+        @Override
+        protected void skipped(long nanos, AssumptionViolatedException e, Description description) {
+            logInfo(description, "skipped", nanos);
+        }
+
+        @Override
+        protected void finished(long nanos, Description description) {
+            logInfo(description, "finished", nanos);
+        }
+    };
+}
