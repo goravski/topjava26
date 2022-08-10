00aba8d3e3a09d8a6558382ebe553cbeb428fb67
Evgen Goravski <evgoravsky@gmail.com>
2022-08-10 22:47:22 +0300

    9_03 Refactored .js by 8.09 patch

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -1,75 +1,39 @@
-let form;
-
-function makeEditable(datatableApi) {
-    ctx.datatableApi = datatableApi;
-    form = $('#detailsForm');
-
-    $(document).ajaxError(function (event, jqXHR, options, jsExc) {
-        failNoty(jqXHR);
+function makeEditable() {
+    $(".delete").click(function () {
+        deleteRow($(this).attr("id"));
     });
-
-    // solve problem with cache in IE: https://stackoverflow.com/a/4303862/548473
-    $.ajaxSetup({cache: false});
 }
 
 function add() {
-    form.find(":input").val("");
+    $("#detailsForm").find(":input").val("");
     $("#editRow").modal();
 }
 
 function deleteRow(id) {
-    if (confirm('Are you sure?')) {
-        $.ajax({
-            url: ctx.ajaxUrl + id,
-            type: "DELETE"
-        }).done(function () {
-            ctx.updateTable();
-            successNoty("Deleted");
-        });
-    }
+    $.ajax({
+        url: ajaxUrl + id,
+        type: "DELETE",
+        success: function () {
+            updateTable();
+        }
+    });
 }
 
-function updateTableByData(data) {
-    ctx.datatableApi.clear().rows.add(data).draw();
+function updateTable() {
+    $.get(ajaxUrl, function (data) {
+        datatableApi.clear().rows.add(data).draw();
+    });
 }
 
 function save() {
+    const form = $("#detailsForm");
     $.ajax({
         type: "POST",
-        url: ctx.ajaxUrl,
-        data: form.serialize()
-    }).done(function () {
-        $("#editRow").modal("hide");
-        ctx.updateTable();
-        successNoty("Saved");
+        url: ajaxUrl,
+        data: form.serialize(),
+        success: function () {
+            $("#editRow").modal("hide");
+            updateTable();
+        }
     });
-}
-
-let failedNote;
-
-function closeNoty() {
-    if (failedNote) {
-        failedNote.close();
-        failedNote = undefined;
-    }
-}
-
-function successNoty(text) {
-    closeNoty();
-    new Noty({
-        text: "<span class='fa fa-lg fa-check'></span> &nbsp;" + text,
-        type: 'success',
-        layout: "bottomRight",
-        timeout: 1000
-    }).show();
-}
-
-function failNoty(jqXHR) {
-    closeNoty();
-    failedNote = new Noty({
-        text: "<span class='fa fa-lg fa-exclamation-circle'></span> &nbsp;Error status: " + jqXHR.status,
-        type: "error",
-        layout: "bottomRight"
-    });
-    failedNote.show()
 }
--- src/main/webapp/resources/js/topjava.users.js
+++ src/main/webapp/resources/js/topjava.users.js
@@ -1,65 +1,42 @@
-const userAjaxUrl = "admin/users/";
-
-// https://stackoverflow.com/a/5064235/548473
-const ctx = {
-    ajaxUrl: userAjaxUrl,
-    updateTable: function () {
-        $.get(userAjaxUrl, updateTableByData);
-    }
-}
-
-function enable(chkbox, id) {
-    var enabled = chkbox.is(":checked");
-//  https://stackoverflow.com/a/22213543/548473
-    $.ajax({
-        url: userAjaxUrl + id,
-        type: "POST",
-        data: "enabled=" + enabled
-    }).done(function () {
-        chkbox.closest("tr").attr("data-user-enabled", enabled);
-        successNoty(enabled ? "Enabled" : "Disabled");
-    }).fail(function () {
-        $(chkbox).prop("checked", !enabled);
-    });
-}
+const ajaxUrl = "admin/users/";
+let datatableApi;
 
 // $(document).ready(function () {
 $(function () {
-    makeEditable(
-        $("#datatable").DataTable({
-            "paging": false,
-            "info": true,
-            "columns": [
-                {
-                    "data": "name"
-                },
-                {
-                    "data": "email"
-                },
-                {
-                    "data": "roles"
-                },
-                {
-                    "data": "enabled"
-                },
-                {
-                    "data": "registered"
-                },
-                {
-                    "defaultContent": "Edit",
-                    "orderable": false
-                },
-                {
-                    "defaultContent": "Delete",
-                    "orderable": false
-                }
-            ],
-            "order": [
-                [
-                    0,
-                    "asc"
-                ]
+    datatableApi = $("#datatable").DataTable({
+        "paging": false,
+        "info": true,
+        "columns": [
+            {
+                "data": "name"
+            },
+            {
+                "data": "email"
+            },
+            {
+                "data": "roles"
+            },
+            {
+                "data": "enabled"
+            },
+            {
+                "data": "registered"
+            },
+            {
+                "defaultContent": "Edit",
+                "orderable": false
+            },
+            {
+                "defaultContent": "Delete",
+                "orderable": false
+            }
+        ],
+        "order": [
+            [
+                0,
+                "asc"
             ]
-        })
-    );
+        ]
+    });
+    makeEditable();
 });
